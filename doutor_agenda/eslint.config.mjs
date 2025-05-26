4ec5807451f0768961234c0219b16d8e39d0e28f
joaovictor53 <victors.rpe@gmail.com>
2025-05-26 11:11:46 -0400

    chore: add prettier and eslint setup

---
+++ doutor_agenda/eslint.config.mjs
@@ -0,0 +1,27 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { FlatCompat } from "@eslint/eslintrc";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["simple-import-sort"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  },
+];
+
+export default eslintConfig;
