d47774efbca621072f55467d8f340a132867c9af
joaovictor53 <victors.rpe@gmail.com>
2025-05-27 16:26:39 -0400

    feat: enhance authentication flow with session check and sign-out functionality

--- doutor_agenda/src/app/dashboard/page.tsx
+++ doutor_agenda/src/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
-"use client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+import { auth } from "@/lib/auth";
+
+import { SignOutButton } from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
   return (
     <div>
       <h1>Dashboard</h1>
+      <h1>{session?.user?.email}</h1>
+      <h1>{session?.user?.name}</h1>
+      <SignOutButton />
     </div>
   );
 };
